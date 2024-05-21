5be08176013c09e754af74007b17184192bb35f5
Petr Belyakov <i@kptlr.ru>
2024-05-22 00:14:24 +0300

    added simpl mvc convert md to html

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as showdown from 'showdown';
+import * as fs from 'fs';
 
-@Controller()
+@Controller('/adrs')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':projectName/:adrId')
+  //@Render('index')
+  getRoot(
+    @Param('projectName') projectName: string,
+    @Param('adrId') adrId: string,
+  ) {
+    const converter = new showdown.Converter();
+    return converter.makeHtml(
+      fs.readFileSync('./adr/' + projectName + '/' + adrId + '.md', 'utf8'),
+    );
   }
 }
